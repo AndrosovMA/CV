aaf431550b538685d0dfb20315bddb1b44c77b0f
AndrosovMA <73516787+AndrosovMA@users.noreply.github.com>
2021-03-14 16:42:56 +0900

    CV_ver1

---
+++ js/app.js
@@ -0,0 +1,68 @@
+/**
+ * Основные данные - шапка резюме
+ */
+document.querySelector('#my-name').innerHTML = `${data.first_name} <span class="color-grey">${data.last_name}</span>`;
+document.querySelector('#job-title').textContent = data.job_title;
+document.querySelector('.profile-data').textContent = data.profile;
+document.querySelector('.summary').textContent = data.summary;
+
+/**
+ * section data
+ */
+let expirienceData = '';
+experience.forEach(item => {
+    expirienceData += `<section class="mb-5">`;
+    expirienceData += ` <h4 class="company-name mb-2">${item.company} - ${item.city}, <i>${item.from} - ${item.to}</i></h4>`;
+    expirienceData += `<h5>${item.position}</h5>`;
+    expirienceData += `<p>${item.description}</p>`;
+    expirienceData += `</section>`;
+});
+document.querySelector('.experience-list').innerHTML = expirienceData;
+
+
+/**
+ * contact data
+ */
+document.querySelector('.contacts-block').innerHTML += `<li><span class="material-icons">phone</span> <a href="tel:${data.phone}">${data.phone_1} </a></li>`
+document.querySelector('.contacts-block').innerHTML += `<li><span class="material-icons">email</span> <a href="mailto:${data.email_1}">${data.email_1}</a></li>`
+document.querySelector('.contacts-block').innerHTML += `<li><span class="material-icons">email</span> <a href="mailto:${data.email_2}">${data.email_2}</a></li>`
+document.querySelector('.contacts-block').innerHTML += `<li><span class="material-icons">phone</span> <a href="callto:${data.skype}">${data.skype}</a></li>`
+document.querySelector('.contacts-block').innerHTML += `<li><span class="material-icons">link</span> <a href="${data.linkedin}">${data.linkedin}</a></li>`
+document.querySelector('.address').innerHTML = `<p>${data.address}</p>`;
+document.querySelector('#my-photo').src = data.photo;
+
+/**
+ * hard skills
+ */
+let skillsData = '';
+data.hard_skills.forEach(item => {
+    skillsData += `<li>${item}</li>`;
+});
+document.querySelector('.skills-list').innerHTML = skillsData;
+
+/**
+ * soft skills
+ */
+let skillsData2 = '';
+data.soft_skills.forEach(item => {
+    skillsData2 += `<li>${item}</li>`;
+});
+document.querySelector('.skills-list2').innerHTML = skillsData2;
+
+/**
+ * education
+ */
+let educationData = '';
+data.education.forEach(item => {
+    educationData += `<li>${item[0]}, ${item[1]}, ${item[2]}, ${item[3]}</li>`;
+});
+document.querySelector('.education-list').innerHTML = educationData;
+
+/**
+ * Achievements
+ */
+// let achievementsData = '';
+// data.achievements.forEach(item => {
+//     achievementsData += `<li>${item},</li>`;
+// });
+// document.querySelector('.achievements-list').innerHTML = achievementsData;
---
+++ js/data.js
@@ -0,0 +1,40 @@
+const data = {
+    "first_name": "Максим",
+    "last_name": "Андросов",
+    "job_title": "Frontend Developer",
+    "photo": "images/cv.jpg",
+    "phone": "+79142585682",
+    "phone_1": "+7(914)258-5682",
+    "email_1": "AndrosovMA@bk.ru",
+    "email_2": "AndrosovMA@gmail.com",
+    "skype": "skype_number",
+    "linkedin": "link_to_linkedin",
+    "address": "г.Санкт-Петербург",
+    "hard_skills": [
+        "HTML и CSS",
+        "Препроцессоры CSS (Sass)",
+        "JavaScript",
+        "jQuery, React.JS, Bootstrap",
+        "Node.js",
+        "ООП в JS",
+        "Инструменты контроля версий (Git, GitHub)",
+        "Базы данных и языки запросов (SQL)"
+    ],
+    "soft_skills": [
+        "Английский - А2 (Pre-Intermediate)",
+        "Знание методологий Agile, Scrum и Kanban",
+        "Коммуникативность",
+        "Самостоятельность и проактивность"
+    ],
+    "education": [
+        ["2004-2009", "ФГАОУ ВО Северо-Восточный федеральный университет имени М.К. Аммосова г.Якутск", "Специальность: Обогащение полезных ископаемых", "Квалификация: Горный инженер"],
+        ["2009-2013", "Московский институт профессиональных инноваций", "Специальность: Финансы и кредит", "Квалификация: Экономист"],
+    ],
+    "achievements": [
+        "Marketer of the Year Academic Scholorship Deans List: 2006-2007, National Merit Scholarship Corporation",
+    ],
+    "profile": "Начать карьеру junior JavaScript developer и за 3 года дорасти до TeamLeader",
+    "summary": "Опыт web-разработки (обучения) около одного года. Имею хорошие аналитические способности. Легко обучаюсь и работаю в коллективе. "
+};
+
+// "HTML для JavaScript разработчиков (HTML, CSS, Flex, Grid, адаптивная верстка, Bootstrap, Gulp, HTML5), JavaScript (Циклы, Массивы, DOM, Object, Touch, API, AJAX), ReactJS (React, Redux, JSX), ООП в JS (Prototype, Super, Extends, Get Set, This, Call, Bind)"
